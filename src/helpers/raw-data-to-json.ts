c14ca9e3ad90612ea201046337f3029c14e119ce
Ion Dormenco <idormenco@gmail.com>
2025-04-10 16:32:19 +0300

    generate MDA data

--- src/helpers/raw-data-to-json.ts
+++ src/helpers/raw-data-to-json.ts
@@ -1,9 +1,9 @@
-import { promises as fs } from "fs";
 import type {
   ElectionDetailsModel,
   ElectionModel,
   GIDData,
 } from "@/common/types";
+import { promises as fs } from "fs";
 import Papa from "papaparse";
 
 import { existsSync } from "node:fs";
@@ -47,8 +47,12 @@ function groupAndSumByGid(
 ): GIDData[] {
   return Object.values(
     data.reduce((acc, row) => {
-      const gid = gidSelector(row).trim();
-      const gidName = gidNameSelector(row).trim();
+      const gid = gidSelector(row)?.trim();
+      const gidName = gidNameSelector(row)?.trim();
+
+      if (!gid) {
+        return acc;
+      }
 
       if (!acc[gid]) {
         const emptyGidData: GIDData = {
@@ -107,71 +111,67 @@ async function transformData() {
     throw new Error("Election not found in public/elections-data/all.json");
   }
 
-  const rawElectionsDataPath = `raw-data/${options.i}.csv`;
+  const rawElectionsDataPath = `./raw-data/${options.i}.csv`;
+  const outputPath = `./public/elections-data/${options.i}.json`;
   if (!existsSync(rawElectionsDataPath)) {
     throw new Error(`Could not find raw CSV file = ${rawElectionsDataPath}`);
   }
 
   const rawElectionsData = await fs.readFile(rawElectionsDataPath, "utf-8");
 
-  Papa.parse<RawElectionsDataRow>(rawElectionsData, {
+  const results = Papa.parse<RawElectionsDataRow>(rawElectionsData, {
     header: true,
     skipEmptyLines: true,
     delimiter: ",",
-    worker: false,
-    async complete(results) {
-      if (results.errors.length) {
-        results.errors.forEach((error, row) =>
-          console.log(`Error #${row}`, error)
-        );
-
-        throw new Error("❌ Error parsing CSV file!");
-      }
-
-      console.log(results);
-
-      const gid0Data = groupAndSumByGid(
-        results.data,
-        (row) => row.GID_0,
-        (row) => row.GID_0_NAME
-      );
-      const gid1Data = groupAndSumByGid(
-        results.data,
-        (row) => row.GID_1,
-        (row) => row.GID_1_NAME
-      );
-      const gid2Data = groupAndSumByGid(
-        results.data,
-        (row) => row.GID_2,
-        (row) => row.GID_2_NAME
-      );
-      const gid3Data = groupAndSumByGid(
-        results.data,
-        (row) => row.GID_3,
-        (row) => row.GID_3_NAME
-      );
-      const gid4Data = groupAndSumByGid(
-        results.data,
-        (row) => row.GID_4,
-        (row) => row.GID_4_NAME
-      );
-
-      const electionData: ElectionDetailsModel = {
-        countryCode: election.countryCode,
-        countryShortName: election.countryShortName,
-        englishTitle: election.englishTitle,
-        startDate: election.startDate,
-        title: election.title,
-        gid0Data,
-        gid1Data,
-        gid2Data,
-        gid3Data,
-        gid4Data,
-      };
-
-      console.log(electionData);
-    },
+    dynamicTyping: true,
   });
+
+  if (results.errors.length) {
+    results.errors.forEach((error, row) => console.log(`Error #${row}`, error));
+
+    throw new Error("❌ Error parsing CSV file!");
+  }
+
+  const gid0Data = groupAndSumByGid(
+    results.data,
+    (row) => row.GID_0,
+    (row) => row.GID_0_NAME
+  );
+  const gid1Data = groupAndSumByGid(
+    results.data,
+    (row) => row.GID_1,
+    (row) => row.GID_1_NAME
+  );
+  const gid2Data = groupAndSumByGid(
+    results.data,
+    (row) => row.GID_2,
+    (row) => row.GID_2_NAME
+  );
+  const gid3Data = groupAndSumByGid(
+    results.data,
+    (row) => row.GID_3,
+    (row) => row.GID_3_NAME
+  );
+  const gid4Data = groupAndSumByGid(
+    results.data,
+    (row) => row.GID_4,
+    (row) => row.GID_4_NAME
+  );
+
+  const electionData: ElectionDetailsModel = {
+    countryCode: election.countryCode,
+    countryShortName: election.countryShortName,
+    englishTitle: election.englishTitle,
+    startDate: election.startDate,
+    title: election.title,
+    gid0Data,
+    gid1Data,
+    gid2Data,
+    gid3Data,
+    gid4Data,
+  };
+
+  await fs.writeFile(outputPath, JSON.stringify(electionData, null, 4), "utf8");
 
   console.log(`✅ Transformation completed in ${end - start}ms`);
 
